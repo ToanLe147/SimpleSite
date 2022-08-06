ba91dbc8a1dae670569dc4ca5e0163c9be3b9a16
Nico Le <normalboyinlife@gmail.com>
2022-08-06 20:21:41 +0300

    Add map and charactors

--- js/Overworld.js
+++ js/Overworld.js
@@ -6,10 +6,36 @@ class Overworld {
     }
 
     init() {
-        const image = new Image();
-        image.onload = () => {
-
+        // Add background to the game
+        const CommonRoom = new Image();
+        CommonRoom.onload = () => {
+            this.ctx.drawImage(
+                CommonRoom, 
+                0, // left cut
+                0, //top cut
+                816, // width of cut
+                384  // height of cut
+            )
         }
-        image.src = ""
-    }
+        CommonRoom.src = "../assets/CommonRoom.png";
+
+        const x = 2
+        const y = 2
+        // Add player to the game
+        const Player = new Image();
+        Player.onload = () => {
+            this.ctx.drawImage(
+                Player, 
+                0, 
+                0,
+                32,
+                64,       
+                x * 48,
+                y * 48,
+                32,
+                64         
+            )
+        }
+        Player.src = "../assets/actors/boy.png"
+    }    
 }
