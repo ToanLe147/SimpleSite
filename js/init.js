a0f14d5dc31a1f17c4ae28db1aa78d811047ab7c
Nico <36053959+ToanLe147@users.noreply.github.com>
2025-01-02 21:49:28 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -45,12 +45,13 @@ let scenes = {
 
 window.addEventListener('load', function () {
     let isMobile = window.matchMedia("(any-pointer:coarse)").matches;
-    if (isMobile)    
-    {
-        document.querySelector('.mobile-device').style.display="inline";        
-    } else {
-        document.querySelector('.mobile-device').style.display="none";
-    }
+    document.querySelector('.mobile-device').style.display="inline"; 
+    // if (isMobile)    
+    // {
+    //     document.querySelector('.mobile-device').style.display="inline";        
+    // } else {
+    //     document.querySelector('.mobile-device').style.display="none";
+    // }
 
     const canvas = document.getElementById('game-canvas');
     const ctx = canvas.getContext('2d')    
